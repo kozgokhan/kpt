4ba7e5ce6793a2e0c071ba8b728439f7a8ba461d
Kaan Ozgokhan <kozgokhan@yahoo.com>
2019-05-26 15:30:16 +0200

    listing and stats functions initated

--- qp.m
+++ qp.m
@@ -1,12 +1,23 @@
-function qp(whereToGo)
-
+function qp(command)
     qpdb = qpf_dbread();
-    [isFound, ~, desiredPath] = qpf_searchName(qpdb, whereToGo);
-    
-    if isFound
-        cd(desiredPath);
+    if strcmp(command, 'qplist')
+        fprintf('+%81s+\n',repmat('-',1,81));
+        fprintf('| %3s | %-20s | %-50s |\n','#', 'name', 'path');
+        fprintf('+%81s+\n',repmat('-',1,81));
+        for i=1:length(qpdb{1,1})
+            fprintf('| %3d | %-20s | %-50s |\n',i, qpdb{1,1}{i}, qpdb{1,2}{i});
+        end
+        fprintf('+%81s+\n',repmat('-',1,81));
+    elseif strcmp(command, 'qpstats')
+        disp('stats are shown')
+        length(qpdb{1,1})
     else
-        disp('there is no such path.');
-    end
+        [isFound, ~, desiredPath] = qpf_searchName(qpdb, command);
 
+        if isFound
+            cd(desiredPath);
+        else
+            disp('there is no such path.');
+        end
+    end
 end
