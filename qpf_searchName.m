8514048b17ea0d76102ffe666f6f0a4f707dc839
Kaan Ozgokhan <kozgokhan@yahoo.com>
2019-05-26 15:29:58 +0200

    small bugfix

--- qpf_searchName.m
+++ qpf_searchName.m
@@ -1,5 +1,6 @@
 function [isFound,i,desiredPath] = qpf_searchName(qpdb, name)
-    itemNum = length(qpdb{1,1}); isFound = 0;
+    itemNum = length(qpdb{1,1}); 
+    isFound = 0; desiredPath = '';
     for i=1:itemNum
         isFound = strcmp(qpdb{1,1}{i}, name);
         if isFound
