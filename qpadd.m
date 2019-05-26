3db078a74fefe4e00d484388b5b9d034e7ec4f60
Kaan Ozgokhan <kozgokhan@yahoo.com>
2019-05-26 13:24:33 +0200

    small update

--- qpadd.m
+++ qpadd.m
@@ -1,10 +1,12 @@
 function qpadd(varargin)
     if length(varargin)==1
         % gui appears
+        % in case this function is called by another gui
         disp('gui appears');
     elseif isempty(varargin)
         % add path via command line
-        disp('add path via command line');
+        pathName = input('enter a name for the path: ','s');
+        disp(pathName)
     else
         % something done wrong
         disp('too many input arguments');
