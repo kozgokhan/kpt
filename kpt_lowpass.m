877ceee2ea7e6fe6d7e59f1db441f3a91ed299af
Kaan Ozgokhan <kozgokhan@yahoo.com>
2019-09-18 16:00:14 +0200

    moved into folder

--- kpt_lowpass.m
+++
@@ -1,9 +0,0 @@
-% https://en.wikipedia.org/wiki/Low-pass_filter
-
-function y = kpt_lowpass(signal, alpha)
-    y = zeros(size(signal));
-    y(1) = signal(1);
-    for i=2:length(signal)
-        y(i) = y(i-1) + alpha*(signal(i) - y(i-1));
-    end
-end
