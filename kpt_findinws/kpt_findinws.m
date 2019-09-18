f37ab092fe9b70b83a5673ffbce9c0f4a55c8482
Kaan Ozgokhan <kozgokhan@yahoo.com>
2019-09-18 16:00:26 +0200

    moved into folder

---
+++ kpt_findinws/kpt_findinws.m
@@ -0,0 +1,11 @@
+function found = kpt_findinws(keyword)
+    allvars = evalin('base','who');
+    
+    n = 1;
+    for i=1:numel(allvars)
+       if strfind(lower(allvars{i}), lower(keyword))>0
+           found(n,1) = allvars(i);
+           n = n+1;
+       end
+    end
+end
